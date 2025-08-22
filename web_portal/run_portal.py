62db0ea99e0fb80621c395c2e75a1a9321f7ba3e
Jun C <kjstudios858@gmail.com>
2025-08-22 06:46:48 +0000

    feat: Update web_portal/run_portal.py to remove unnecessary imports and paths

--- web_portal/run_portal.py
+++ web_portal/run_portal.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 
-import sys
-from pathlib import Path
+# import sys
+# from pathlib import Path
 
-# Add the web_portal folder to the Python path
-project_root = Path(__file__).resolve().parent
-web_portal_path = project_root / "web_portal"
-sys.path.insert(0, str(web_portal_path))
+# # Add the web_portal folder to the Python path
+# project_root = Path(__file__).resolve().parent
+# web_portal_path = project_root / "web_portal"
+# sys.path.insert(0, str(web_portal_path))
 
 # Import and run the Flask app
 from app import app
